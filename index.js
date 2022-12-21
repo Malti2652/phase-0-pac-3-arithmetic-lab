9b4c82567e85c824cc910104d8900935233439bc
Eddington Rowellis <eddingtonibi@gmail.com>
2022-12-21 22:52:43 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,36 @@
+a = Math.floor(Math.random()*1000);
+b = Math.floor(Math.random()*1000);
+let a = 3;
+let b = 4;
+function add(a,b) {
+  return (a + b);
+  a += 1;
+  
+}
+
+function subtract(a,b) {
+  return (a - b);
+}
+
+function multiply(a,b) {
+  return (a * b);
+}
+
+function divide(a,b) {
+  return (a / b);
+}
+function increment(a) {
+  return a += 1;
+}
+
+function decrement(a) {
+  return a -= 1;
+}
+
+function makeInt(a) {
+  return parseInt(a,10);
+}
+
+function preserveDecimal(a) {
+  return parseFloat(a)
+}
